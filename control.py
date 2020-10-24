af2b9224415f66dd885e21265bb2e222c135edfd
Vincent Mai <46911814+vincentdmai@users.noreply.github.com>
2020-10-24 14:31:35 -0400

    Addressed Infinite Loop in consume

--- control.py
+++ control.py
@@ -1,13 +1,12 @@
+#!/usr/bin/python3
 import pika
 import pymongo
 import time
 import sys
-import json
 import pprint
 import copy
 
-#TODO: Initialization of User Login
-#Change these later too
+# Initialization of User Login
 username = 'justmalvince'
 password = 'team23'
 
@@ -17,18 +16,15 @@ password = 'team23'
 def rabbit_set_up(IP, PORT) :
     credentials = pika.PlainCredentials(username, password)
 
-    #TODO
-    #change localhost later to the repo IP
-    #Replace the lower lines with these when we figure out the port and ip and credentials stuff
+    # Setting RabbitMQ connection based on credentials and IP/PORT
     parameters = pika.ConnectionParameters(IP, PORT, '/', credentials)
     connection = pika.BlockingConnection(parameters)
     
-    #
-    #
     '''
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost'))
     '''
+    
     channel = connection.channel()
     print("[Ctrl 01] - Connecting to RabbitMQ instance on " + IP + " with port " + str(PORT))
  
@@ -38,17 +34,17 @@ def rabbit_set_up(IP, PORT) :
     channel.exchange_declare(exchange='Squires', exchange_type='direct')
     
     #Creating food queue and binding to squires exchange
-    food_queue = channel.queue_declare(queue='Food', exclusive=True)
+    channel.queue_declare(queue='Food', exclusive=True)
     channel.queue_bind(exchange='Squires', queue='Food', routing_key='Food')
     listPairExQu.append("Squires:Food")
  
     #Creating meetings queue and binding to squires exchange
-    meetings_queue = channel.queue_declare(queue='Meetings', exclusive=True)
+    channel.queue_declare(queue='Meetings', exclusive=True)
     channel.queue_bind(exchange='Squires', queue='Meetings', routing_key='Meetings')
     listPairExQu.append("Squires:Meetings")
  
     #Creating rooms queue and binding to squires exchange
-    rooms_queue = channel.queue_declare(queue='Rooms', exclusive=True)
+    channel.queue_declare(queue='Rooms', exclusive=True)
     channel.queue_bind(exchange='Squires', queue='Rooms', routing_key='Rooms')
     listPairExQu.append("Squires:Rooms")
  
@@ -56,12 +52,12 @@ def rabbit_set_up(IP, PORT) :
     channel.exchange_declare(exchange='Goodwin', exchange_type='direct')
     
     #Creating classrooms queue and binding to goodwin exchange
-    classrooms_queue = channel.queue_declare(queue='Classrooms', exclusive=True)
+    channel.queue_declare(queue='Classrooms', exclusive=True)
     channel.queue_bind(exchange='Goodwin', queue='Classrooms', routing_key='Classrooms')
     listPairExQu.append("Goodwin:Classrooms")
  
     #Creating auditorium queue and binding to goodwin exchange
-    auditorium_queue = channel.queue_declare(queue='Auditorium', exclusive=True)
+    channel.queue_declare(queue='Auditorium', exclusive=True)
     channel.queue_bind(exchange='Goodwin', queue='Auditorium', routing_key='Auditorium')
     listPairExQu.append("Goodwin:Auditorium")
  
@@ -69,17 +65,17 @@ def rabbit_set_up(IP, PORT) :
     channel.exchange_declare(exchange='Library', exchange_type='direct')
     
     #Creating noise queue and binding to library exchange
-    noise_queue = channel.queue_declare(queue='Noise', exclusive=True)
+    channel.queue_declare(queue='Noise', exclusive=True)
     channel.queue_bind(exchange='Library', queue='Noise', routing_key='Noise')
     listPairExQu.append("Library:Noise")
  
     #Creating seating queue and binding to library exchange
-    seating_queue = channel.queue_declare(queue='Seating', exclusive=True)
+    channel.queue_declare(queue='Seating', exclusive=True)
     channel.queue_bind(exchange='Library', queue='Seating', routing_key='Seating')
     listPairExQu.append("Library:Seating")
  
     #Creating wishes queue and binding to library exchange
-    wishes_queue = channel.queue_declare(queue='Wishes', exclusive=True)
+    channel.queue_declare(queue='Wishes', exclusive=True)
     channel.queue_bind(exchange='Library', queue='Wishes', routing_key='Wishes')
     listPairExQu.append("Library:Wishes")
  
@@ -136,7 +132,7 @@ if __name__ == '__main__':
                 if arg == '-rip':
                     repo_IP = sys.argv[i+1]
                 elif arg == '-rport':
-                    repo_PORT = sys.argv[i+1]
+                    repo_PORT = int(sys.argv[i+1])
             
         # If invalid parameter flags, then break with ERROR
         if not repo_IP or not repo_PORT:
@@ -186,21 +182,20 @@ if __name__ == '__main__':
                         'Subject': subject,
                         'Message': message
                     }
+                
+                # Used copy to obtain a clean 'JSON' of post before it is modified by MongoDB through posts.insert_one()
                 printPost = copy.copy(post)
 
 
                 posts = db.posts
                 post_id = posts.insert_one(post)
 
-
-                #TODO
                 # Printing Input Command String
                 print("[Ctrl 05] – Inserted command into MongoDB: " + inp)
 
                 # Printing Post in Pretty Print JSON format
                 pprint.pprint(printPost, indent=2)
                 
-                #TODO
                 # Establishes basic publish such that a body is 'published' within the specified queue for later consumption.
                 channel.basic_publish(exchange = place, routing_key = subject, body=message)
                 print("[Ctrl 06] - Produced message '" + message + "' on <" + place.upper() + ":" + subject.upper() + ">")
@@ -224,25 +219,33 @@ if __name__ == '__main__':
                         'MsgID': msgID,
                         'Subject': subject
                     }
+
+                # Used copy to obtain a clean 'JSON' of post before it is modified by MongoDB through posts.insert_one()
                 printPost = copy.copy(post)
                 posts = db.posts
                 post_id = posts.insert_one(post)
 
-                #TODO
                 # Printing Input Command String
                 print("[Ctrl 05] – Inserted command into MongoDB: " + inp)
 
                 # Printing Post in Pretty Print JSON format
                 pprint.pprint(printPost, indent=2)
 
-                #TODO
                 # Consume from RabbitMQ. Establishing basic consume connection and then accessing callback to obtain the message from
                 # a specific queue.
                 # The message is then printed out on the command line. 
-                channel.basic_consume(on_message_callback=callback, queue=subject, auto_ack=True)
-                
-                channel.start_consuming()
-                print("[Ctrl 07] - Consumed message '" + CALLBACK_BODY + "' on <" + place.upper() + ":" + subject.upper() + ">")
+
+                # Obtain current queue state extracted from the commandline to check if there are any pre-existing messages within the queue
+                # If there is, access the basic consumption method to callback, else, do NOT consume since it will put the program into an infinite loop
+                queue_state = channel.queue_declare(subject, durable=True, passive=True)
+                queue_empty = queue_state.method.message_count == 0
+                if not queue_empty:
+                    channel.basic_consume(on_message_callback=callback, queue=subject, auto_ack=True)
+                    
+                    channel.start_consuming()
+                    print("[Ctrl 07] - Consumed message '" + CALLBACK_BODY + "' on <" + place.upper() + ":" + subject.upper() + ">")
+                else:
+                    print("[Ctrl 07] - No Messages in " + "<" + place.upper() + ":" + subject.upper() + ">")
 
     
 
